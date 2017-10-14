ccd8744e2dff8ff396c41c3daed05d00b2a458fd
Abdul Nassar <abdulnasarkvr@gmail.com>
2017-10-14 19:18:49 +0400

    seller module added

---
+++ NscSeller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NscSeller.Startup))]
+namespace NscSeller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
